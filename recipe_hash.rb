581aa9715ae43e2569633dd5ba17859b7f13ea5b
Carolinemay <czhou@students.chccs.k12.nc.us>
2018-07-26 15:39:07 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -23,7 +23,7 @@ recipe_ingredients(perfect_10_recipe)
 perfect_10_recipe = {:almond_flour => "2 cup", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", :baking_powder => "1/2 tea spoon", :baking_soda => "1/4 teaspoon", :xanthan_Gum => "1/2 tea spoon", :slivered_almonds_lightly_toasted => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"}
 
 
-#2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
+#2: Write code that returns the amount of chocolate chips from perfect_10  ingredients that is needed in the recipe.
 def amount_of_chocolate_chips(perfect_10_recipe)
 end
 
