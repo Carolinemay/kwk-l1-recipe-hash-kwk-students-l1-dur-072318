4a9186da3867d12130d617fe3e7a81552a95120c
Carolinemay <czhou@students.chccs.k12.nc.us>
2018-07-26 14:59:55 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -12,14 +12,14 @@
 #  - olive oil - 1/2 cup
 #  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
-perfect_10_recipe = {:almond_flour => "2 cup", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", ::baking powder => "1/2 tea spoon", :baking_soda => "1/4 teaspoon", :xanthan_Gum => "1/2 tea spoon", :slivered_almonds_lightly_toasted => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"}
+perfect_10_recipe = {:almond_flour => "2 cup", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", :baking_powder => "1/2 tea spoon", :baking_soda => "1/4 teaspoon", :xanthan_Gum => "1/2 tea spoon", :slivered_almonds_lightly_toasted => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"}
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
 def recipe_ingredients(perfect_10_recipe)
-  perfect_10_recipe = {:almond_flour => "2 cup", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", ::baking powder => "1/2 tea spoon", :baking_soda => "1/4 teaspoon", :xanthan_Gum => "1/2 tea spoon", :slivered_almonds_lightly_toasted => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"}
+  perfect_10_recipe = {:almond_flour => "2 cup", :gluten_free_whole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", :baking_powder => "1/2 tea spoon", :baking_soda => "1/4 teaspoon", :xanthan_Gum => "1/2 tea spoon", :slivered_almonds_lightly_toasted => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"}
 return value
 end
-
+recipe_ingredints(perfect_10_recipe)
 #2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
 def amount_of_chocolate_chips(perfect_10_recipe)
 end
